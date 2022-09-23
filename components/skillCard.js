b0b419e888c40b9785433fe85414d39e2e8f0697
nathenjo <nateselectronicrepair@gmail.com>
2022-09-23 17:46:01 -0400

    Finished skill-card style and behavior

--- components/skillCard.js
+++ components/skillCard.js
@@ -5,17 +5,27 @@ export default function SkillCard(props) {
     
     const { imageSrc, cardTitle, cardCaption, projects } = props;
 
-    const [cardFlip, setCardFlip] = useState('front');
+    const [cardFlip, setCardFlip] = useState(false);
+    const [frontRotate, setFrontRotate] = useState('0deg');
+    const [backRotate, setBackRotate] = useState('-180deg');
 
     useEffect(() => {
-        if (cardFlip == 'front') {
-            
+        if (cardFlip == false) {
+            setFrontRotate('0deg');
+            setBackRotate('-180deg')
+        } else if (cardFlip == true) {
+            setFrontRotate('180deg');
+            setBackRotate('0deg')
         }
-    }, [])
-    
+    }, [cardFlip])
+
+    const flipCard = () => {
+        setCardFlip(!cardFlip);
+    }
+
     return (
-        <div className='skill-card'>
-            <div class='card-front'>
+        <div onClick={flipCard} className='skill-card'>
+            <div style={{'transform': `rotateY(${frontRotate})`}} class='card-front'>
                 <Image
                     src={imageSrc}
                     className='card-img'
@@ -25,7 +35,7 @@ export default function SkillCard(props) {
                 <h2>{cardTitle}</h2>
                 <p>{cardCaption}</p>
             </div>
-            <div class='card-back'>
+            <div style={{'transform': `rotateY(${backRotate})`}} class='card-back'>
                 <h2>Projects</h2>
                 <ul>
                     {projects.length > 0 && projects.map((item, index) => {
